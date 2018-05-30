f8edea463345dec3b953d662ccdb9481df6d16f0
Tralfazz <matan.yesharim@gmail.com>
2018-05-30 22:29:38 +0300

    added room, TODO room.cpp

---
+++ Room.cpp
@@ -0,0 +1,73 @@
+#include "Room.h"
+
+Room::Room(int id, User * admin, std::string name, int maxUsers, int questionNo, int questionTime)
+	: _id(id), _admin(admin),_name(name), _maxUsers(maxUsers), _questionNo(questionNo),_questionTime(questionTime)
+{
+	_users.push_back(admin);
+}
+
+std::string Room::getUsersAsString(std::vector<User*> usersList, User * excludeUser)
+{
+	std::string s;
+
+	s += "\n";
+	for (auto user : usersList)
+	{
+		if (user != excludeUser)
+		{
+			s += (user->getUsername + ",");
+		}
+	}
+	s += "\n";
+	return s;
+}
+
+void Room::sendMessage(std::string)
+{
+
+}
+
+void Room::sendMessage(User *, std::string)
+{
+}
+
+bool Room::joinRoom(User *)
+{
+	return false;
+}
+
+void Room::leaveRoom(User *)
+{
+}
+
+int Room::closeRoom(User *)
+{
+	return 0;
+}
+
+std::vector<User*> Room::getUsers()
+{
+	return std::vector<User*>();
+}
+
+std::string Room::getUsersListMesasage()
+{
+	return std::string();
+}
+
+int Room::getQuestionsNo()
+{
+	return 0;
+}
+
+int Room::getId()
+{
+	return 0;
+}
+
+std::string Room::getName()
+{
+	return std::string();
+}
+
+
--- Room.h
+++ Room.h
@@ -1,7 +1,30 @@
 #pragma once
-
+#include "User.h"
 
 class Room
 {
+	private:
+		std::vector<User*> _users;
+		User* _admin;
+		int _maxUsers;
+		int _questionTime;
+		int _questionNo;
+		std::string _name;
+		int _id;
+	
+		std::string getUsersAsString(std::vector<User*>, User*);
+		void sendMessage(std::string);
+		void sendMessage(User*, std::string);
 
+
+	public:
+		Room(int, User*, std::string, int, int, int);
+		bool joinRoom(User*);
+		void leaveRoom(User*);
+		int closeRoom(User*);
+		std::vector<User*> getUsers();
+		std::string getUsersListMesasage();
+		int getQuestionsNo();
+		int getId();
+		std::string getName();
 };
