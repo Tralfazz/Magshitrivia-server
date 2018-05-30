4c916d8dfb7453c915c351ef5519597998b85d8a
ofirnaca <ofirnaca@gmail.com>
2018-05-30 22:12:55 +0300

    added client and server now kinda works

--- Protocol.h
+++ Protocol.h
@@ -3,53 +3,61 @@
 
 namespace Protocol 
 {
-	enum Request
+	const int PORT = 8080;
+
+	class Request
 	{
-		SIGN_IN = 200,
-		SIGN_OUT = 201,
-		SIGN_UP = 203,
+		public:
 
-		EXISTING_ROOMS = 205,
-		USERS_FROM_ROOM = 207,
+			static const int SIGN_IN = 200;
+			static const int SIGN_OUT = 201;
+			static const int SIGN_UP = 203;
 
-		JOIN_ROOM = 209,
-		LEAVE_ROOM = 211,
+			static const int EXISTING_ROOMS = 205;
+			static const int USERS_FROM_ROOM = 207;
 
-		CREATE_NEW_ROOM = 213,
-		CLOSE_ROOM = 215,
+			static const int JOIN_ROOM = 209;
+			static const int LEAVE_ROOM = 211;
 
-		BEGIN_GAME = 217,
+			static const int CREATE_NEW_ROOM = 213;
+			static const int CLOSE_ROOM = 215;
 
-		GAME_CLIENT_ANSWER = 219,
+			static const int BEGIN_GAME = 217;
 
-		LEAVE_GAME = 222,
-		BEST_SCORES = 223,
+			static const int GAME_CLIENT_ANSWER = 219;
 
-		PERSONAL_MODE = 225,
+			static const int LEAVE_GAME = 222;
+			static const int BEST_SCORES = 223;
 
-		EXIT_APP = 299
-	};
+			static const int PERSONAL_MODE = 225;
+			
+			static const int EXIT_APP = 299;
+	};	
 
-	enum Response 
+	
+
+	class Response 
 	{
-		SIGN_IN = 102,
-		SIGN_UP = 104,
+		public:
 
-		EXISTING_ROOMS = 106,
-		USERS_FROM_ROOM = 108,
-
-		JOIN_ROOM = 110,
-		LEAVE_ROOM = 112,
-
-		CREATE_NEW_ROOM = 114,
-		CLOSE_ROOM = 116,
-
-		QUESTION = 118,
-		IS_ANSWER_CORRECT = 120,
-		END_GAME = 121,
-
-		BEST_SCORES = 124,
-
-		PERSONAL_MODE = 126
+			static const int SIGN_IN = 102;
+			static const int SIGN_UP = 104;
+			
+			static const int EXISTING_ROOMS = 106;
+			static const int USERS_FROM_ROOM = 108;
+			 
+			static const int JOIN_ROOM = 110;
+			static const int LEAVE_ROOM = 112;
+			
+			static const int CREATE_NEW_ROOM = 114;
+			static const int CLOSE_ROOM = 116;
+			 
+			static const int QUESTION = 118;
+			static const int IS_ANSWER_CORRECT = 120;
+			static const int END_GAME = 121;
+			 
+			static const int BEST_SCORES = 124;
+			
+			static const int PERSONAL_MODE = 126;
 	};
 }
--- User.h
+++ User.h
@@ -5,26 +5,27 @@
 #include "Room.h"
 #include "Game.h"
 
+
 class User
 {
-private:
-	std::string _username;
-	Room* _currRoom;
-	Game* _currGame;
-	SOCKET _sock;
-
-public:
-	User(std::string, SOCKET);
-	void send(std::string);
-	std::string getUsername();
-	SOCKET getSocket();
-	Room* getRoom();
-	Game* getGame();
-	void setGame(Game*);
-	void clearRoom();
-	bool createRoom(int, std::string, int, int, int);
-	bool joinRoom(Room*);
-	void leaveRoom();
-	int closeRomm();
-	bool leaveGame();
+	private:
+		std::string _username;
+		Room* _currRoom;
+		Game* _currGame;
+		SOCKET _sock;
+	
+	public:
+		User(std::string, SOCKET);
+		void send(std::string);
+		std::string getUsername();
+		SOCKET getSocket();
+		Room* getRoom();
+		Game* getGame();
+		void setGame(Game*);
+		void clearRoom();
+		bool createRoom(int, std::string, int, int, int);
+		bool joinRoom(Room*);
+		void leaveRoom();
+		int closeRomm();
+		bool leaveGame();
 };
